baacdf44b82afaef9fd46d68b73022eef02caac3
teopteop <teopteop0718@gmail.com>
2025-06-15 00:39:55 +0900

    feat: 커스텀예외, 핸들러 추가

--- src/main/java/com/example/taskflow/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/taskflow/common/exception/GlobalExceptionHandler.java
@@ -36,4 +36,9 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, ex.getMessage(), null));
 	}
 
+	@ExceptionHandler(TodoNotFoundException.class)
+	public ResponseEntity<ApiResponse<Void>> handlerTodoNotFoundException(TodoNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, ex.getMessage(), null));
+	}
+
 }
