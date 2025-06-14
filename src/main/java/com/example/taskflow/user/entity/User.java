14e6f55155532c156694f0217902bc0042cf2ce2
teopteop <teopteop0718@gmail.com>
2025-06-15 00:04:34 +0900

    feat: 테스트용 기능 추가

--- src/main/java/com/example/taskflow/user/entity/User.java
+++ src/main/java/com/example/taskflow/user/entity/User.java
@@ -9,11 +9,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Setter // 테스트용
+@Getter
 @Table(name = "users")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// @NoArgsConstructor(access = AccessLevel.PROTECTED) //테스트를 위해 막아둠
 public class User extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
