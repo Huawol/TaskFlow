41203d6bc268f904688fb7f0e3eda14ed8e673c5
teopteop <teopteop0718@gmail.com>
2025-06-16 11:04:25 +0900

    feat: 테스트 용 임시 클래스 추가

---
+++ src/main/java/com/example/taskflow/task/dto/response/CommentSimpleResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.taskflow.task.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.example.taskflow.comment.entity.Comment;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommentSimpleResponseDto {
+	private Long id;
+
+	private String content;
+
+	private LocalDateTime createdAt;
+
+	public static CommentSimpleResponseDto from(Comment comment) {
+		return new CommentSimpleResponseDto(comment.getId(), comment.getContent(), comment.getCreatedAt());
+	}
+}
