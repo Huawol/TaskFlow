9faf859a0063ebc56e192e7b9d9913047958e110
teopteop <teopteop0718@gmail.com>
2025-06-17 15:50:46 +0900

    feat: 검증 어노테이션 추가

--- src/main/java/com/example/taskflow/task/dto/request/TaskCreateRequestDto.java
+++ src/main/java/com/example/taskflow/task/dto/request/TaskCreateRequestDto.java
@@ -20,14 +20,11 @@ public class TaskCreateRequestDto {
 	@NotBlank(message = "내용을 입력해주세요.")
 	private String content;
 
-	//null 값 허용할지 고민중
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyyMMdd")
+	@NotBlank(message = "마감일을 설정해주세요.")
 	private LocalDate deadline;
 
-	//초기값이 Todo 이므로 생성 시 미사용 필드
-	// private String status;
-
-	//초기값이 세팅 되어 있음 -> 추후 프론트와 연동시 초기세팅이 있을거임
+	@NotBlank(message = "중요도를 설정해주세요")
 	private String priority;
 
 }
