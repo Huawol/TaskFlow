796f44a9bda5a0f2fb76a6133cbff42a3155ead6
teopteop <teopteop0718@gmail.com>
2025-06-17 20:44:23 +0900

    fix: user 도메인과 login 도메인의 병합내용중 충돌사항 수정

---
+++ src/main/java/com/example/taskflow/user/dto/SignupResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.taskflow.user.dto;
+
+
+import com.example.taskflow.user.entity.User;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class SignupResponseDto {
+
+	private final Long id;
+	private final String email;
+	private final String name;
+	private final LocalDateTime createdAt;
+
+	public SignupResponseDto(User user) {
+		this.id = user.getId();
+		this.email = user.getEmail();
+		this.name = user.getUserName();
+		this.createdAt = user.getCreatedAt();
+	}
+}
