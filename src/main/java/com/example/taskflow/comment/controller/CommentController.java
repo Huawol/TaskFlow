2ac6ade31832e24cf902ad6268a2302e3ce6e5d0
teopteop <teopteop0718@gmail.com>
2025-06-18 16:46:08 +0900

    fix: 세션 기반 로직 -> 프로젝트의 토큰기반으로 전체 수정

--- src/main/java/com/example/taskflow/comment/controller/CommentController.java
+++ src/main/java/com/example/taskflow/comment/controller/CommentController.java
@@ -4,40 +4,38 @@ import com.example.taskflow.comment.dto.request.CommentCreateRequestDto;
 import com.example.taskflow.comment.dto.response.FindAllCommentResponseDto;
 import com.example.taskflow.comment.dto.request.UpdateCommentRequestDto;
 import com.example.taskflow.comment.service.CommentService;
-import com.example.taskflow.user.entity.User;
-import jakarta.servlet.http.HttpSession;
+import com.example.taskflow.security.dto.AuthUserDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("/api/tasks/comments")
 @AllArgsConstructor
 public class CommentController {
     private final CommentService commentService;
 
-    private User getLoginUser(HttpSession session) {
-        User loginUser = (User) session.getAttribute("loginUser");
-        if (loginUser == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
-        }
-        return loginUser;
-    }
+    // private User getLoginUser(HttpSession session) {
+    //     User loginUser = (User) session.getAttribute("loginUser");
+    //     if (loginUser == null) {
+    //         throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
+    //     }
+    //     return loginUser;
+    // }
 
 
     @PostMapping
     public ResponseEntity<Long> saveComment(
             @RequestBody CommentCreateRequestDto requestDto,
-            HttpSession session
+            @AuthenticationPrincipal AuthUserDto authUserDto
     ) {
 
-        User loginUser = getLoginUser(session);
         Long commentId = commentService.saveComment(
-                loginUser.getId(),
+                authUserDto.getId(),
                 requestDto.getTaskId(),
                 requestDto.getContent()
         );
@@ -49,21 +47,18 @@ public class CommentController {
     public ResponseEntity<Long> updateCommentById(
             @PathVariable Long commentId,
             @RequestBody UpdateCommentRequestDto requestDto,
-            HttpSession session
+            @AuthenticationPrincipal AuthUserDto authUserDto
     ) {
-        User loginUser = getLoginUser(session);
-
-        commentService.updateCommentById(loginUser.getId(), commentId, requestDto);
+        commentService.updateCommentById(authUserDto.getId(), commentId, requestDto);
         return new ResponseEntity<>(commentId, HttpStatus.OK);
     }
 
     @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteCommentById(
             @PathVariable Long commentId,
-            HttpSession session
+        @AuthenticationPrincipal AuthUserDto authUserDto
     ) {
-        User loginUser = getLoginUser(session);
-        commentService.deleteCommentById(loginUser.getId(), commentId);
+        commentService.deleteCommentById(authUserDto.getId(), commentId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
