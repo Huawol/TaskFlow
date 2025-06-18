e4026a8d44b7007147b7a52010d8b7177b477fc9
teopteop <teopteop0718@gmail.com>
2025-06-18 21:54:45 +0900

    feat: 토큰 블랙리스트 로직 구현
    로그아웃, 회원탈퇴 시 토큰을 블랙리스트로 저장 처음세팅된 만료시간이 지날때 매 시 마다 삭제됨, 토큰블랙리스트와 헤더의 블랙리스트를 비교하여 필터에서 처리

---
+++ src/main/java/com/example/taskflow/security/entity/TokenBlacklist.java
@@ -0,0 +1,32 @@
+package com.example.taskflow.security.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="token_blacklist")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TokenBlacklist {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String token;
+
+	//토큰 만료시간
+	private LocalDateTime expiredAt;
+
+	public TokenBlacklist(String token, LocalDateTime expiredAt) {
+		this.token = token;
+		this.expiredAt = expiredAt;
+	}
+}
--- src/main/java/com/example/taskflow/user/controller/UserController.java
+++ src/main/java/com/example/taskflow/user/controller/UserController.java
@@ -13,6 +13,7 @@ import com.example.taskflow.user.dto.UserDeleteRequestDto;
 import com.example.taskflow.user.service.UserService;
 import com.example.taskflow.user.service.UserServiceImpl;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -55,18 +56,25 @@ public class UserController {
     //- 클라이언트가 전달한 비밀번호를 검증 후 soft delete 처리
     //- 인증된 사용자 본인만 가능
     @DeleteMapping("/delete")
-    public ResponseEntity<Void> deleteUser(
+    public ResponseEntity<ApiResponse<Void>> deleteUser(
         @AuthenticationPrincipal AuthUserDto authUserDto,
-        @RequestBody UserDeleteRequestDto requestDto) {
+        @RequestBody UserDeleteRequestDto requestDto,
+        HttpServletRequest request) {
+        //request 에서 토큰 값 가져오기
+        String token = jwtUtil.substringToken(request.getHeader("Authorization"));
 
-        userService.deleteUser(authUserDto.getEmail(), requestDto.getPassword());
-        return ResponseEntity.noContent().build();
+        userService.deleteUser(token, authUserDto.getEmail(), requestDto.getPassword());
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new ApiResponse<>(true, "정삭적으로 회원탈퇴가 완료되었습니다.", null));
     }
 
     //로그아웃 API
     //- 서버 처리 없음, 클라이언트가 토큰을 삭제
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout() {
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        //request 에서 토큰 값 가져오기
+        String token = jwtUtil.substringToken(request.getHeader("Authorization"));
+        userService.logout(token);
         return ResponseEntity.ok().build();
     }
 }
